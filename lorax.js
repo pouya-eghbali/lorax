e0fc818aac5b836be93af772bc0783fd3364b7c5
Pouya Eghbali <persian.writer@gmail.com>
2019-11-12 19:43:24 +0100

    Add menu to wrapper, not to body

--- lorax.js
+++ lorax.js
@@ -36,7 +36,7 @@ export default class Lorax {
     this.removeBtn.innerText = 'Remove'
     this.menu.appendChild(this.addBtn)
     this.menu.appendChild(this.removeBtn)
-    document.body.appendChild(this.menu)
+    this.el.appendChild(this.menu)
   }
   installMenu() {
     this.el.addEventListener('contextmenu', event => {
