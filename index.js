89117c39b5336a29a0a7a7b20a20af8d08279416
Pouya Eghbali <persian.writer@gmail.com>
2019-11-12 19:53:28 +0100

    UMD

--- index.js
+++ index.js
@@ -48,8 +48,8 @@
       this.el.addEventListener('contextmenu', event => {
         event.preventDefault();
         this.target = event.target.closest('.lorax-node');
-        const { clientX, clientY } = event;
-        this.openMenu(clientX, clientY);
+        const { offsetX, offsetY } = event;
+        this.openMenu(offsetX, offsetY);
       });
       this.addBtn.addEventListener('click', event => {
         this.addNodeToTarget();
